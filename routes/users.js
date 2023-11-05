9079e83166fb9475a598a5fd1736da0580f36ac7
tri <trisulipras@gmail.com>
2023-11-05 08:50:37 +0700

    TBA-01: Create User Feature
    - Create a script to handle user collection
    - Adding new route to users router to make a http request to create a new user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const dbUser = require('../db/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/', async (req, res) => {
+  const result = await dbUser.create(req.body);
+
+  res.send({
+    data: result,
+    meta: {
+      message: 'success',
+      statusCode: 201,
+    }
+  });
 });
 
 module.exports = router;
