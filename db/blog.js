31aca5e5e88e87fbfb2054cb38ce76ca7fb6131d
tri <trisulipras@gmail.com>
2023-11-05 10:30:59 +0700

    TBA-02: Adding Schema & Schema Rules in Every db Collections
    - Adding schema & rules in users, blogs, & schedules model collection

--- db/blog.js
+++ db/blog.js
@@ -1,5 +1,98 @@
 const dotenv = require('dotenv');
 const { client} = require("./index");
+const user = require('./user');
 
 dotenv.config();
 
+/**
+ * Define the schema structure for blogs collection
+ *
+ * @type {
+ *  {
+ *    title: string,
+ *    content: string
+ *    creator: user.Schema,
+ *    dob: Date,
+ *    updated_at: Date|null,
+ *    created_at: Date|null,
+ *    deleted_at: Date|null,
+ *   }
+ * }
+ */
+const Schema = {
+  title: '',
+  content: '',
+  creator: user.Schema,
+  created_at: null,
+  updated_at: null,
+  deleted_at: null
+};
+
+/**
+ * Define the schema rules for blogs collection
+ *
+ * @type {
+ *  {
+ *    description: {
+ *      type: string,
+ *      required: boolean
+ *    },
+ *    title: {
+ *      min: number,
+ *      max: number,
+ *      required: boolean
+ *    },
+ *    creator: {
+ *      type: string,
+ *      exists: string[],
+ *      required: boolean
+ *    },
+ *    createdAt: {
+ *      type: string,
+ *      required: boolean
+ *    },
+ *    updatedAt: {
+ *      type: string,
+ *      required: boolean
+ *    }
+ *    deletedAt: {
+ *      type: string,
+ *      required: boolean
+ *    },
+ *   }
+ *  }
+ */
+const SchemaRules = {
+  title: {
+    type: 'string',
+    required: true,
+    min: 15,
+    max: 50,
+  },
+  content: {
+    type: 'string',
+    required: true,
+  },
+  creator: {
+    type: 'collection_id',
+    required: true,
+    exists: ['users']
+  },
+  createdAt: {
+    type: 'date',
+    required: false,
+  },
+  updatedAt: {
+    type: 'date',
+    required: false,
+  },
+  deletedAt: {
+    type: 'date',
+    required: false,
+  }
+};
+
+module.exports = {
+  Schema,
+  Rules: SchemaRules
+}
--- db/schedule.js
+++ db/schedule.js
@@ -1,4 +1,99 @@
 const dotenv = require('dotenv');
 const { client} = require("./index");
+const user = require('./user');
 
 dotenv.config();
+
+/**
+ * Define the schema structure for blogs collection
+ *
+ * @type {
+ *  {
+ *    title: string,
+ *    content: string
+ *    creator: user.Schema,
+ *    dob: Date,
+ *    updated_at: Date|null,
+ *    created_at: Date|null,
+ *    deleted_at: Date|null,
+ *   }
+ * }
+ */
+const Schema = {
+  title: '',
+  description: '',
+  creator: user.Schema,
+  created_at: null,
+  updated_at: null,
+  deleted_at: null
+};
+
+/**
+ * Define the schema rules for schedules collection
+ *
+ * @type {
+ *  {
+ *    description: {
+ *      type: string,
+ *      required: boolean
+ *    },
+ *    title: {
+ *      type: string,
+ *      min: number,
+ *      max: number,
+ *      required: boolean
+ *    },
+ *    creator: {
+ *      type: string,
+ *      exists: string[],
+ *      required: boolean
+ *    },
+ *    createdAt: {
+ *      type: string,
+ *      required: boolean
+ *    },
+ *    updatedAt: {
+ *      type: string,
+ *      required: boolean
+ *    }
+ *    deletedAt: {
+ *      type: string,
+ *      required: boolean
+ *    },
+ *   }
+ *  }
+ */
+const SchemaRules = {
+  title: {
+    type: 'string',
+    required: true,
+    min: 15,
+    max: 50,
+  },
+  description: {
+    type: 'string',
+    required: true,
+  },
+  creator: {
+    type: 'collection_id',
+    required: true,
+    exists: ['users']
+  },
+  createdAt: {
+    type: 'date',
+    required: false,
+  },
+  updatedAt: {
+    type: 'date',
+    required: false,
+  },
+  deletedAt: {
+    type: 'date',
+    required: false,
+  }
+};
+
+module.exports = {
+  Schema,
+  Rules: SchemaRules
+}
