7fedd5b38c81ebcb60a42e2c6b0a74768ca68baa
tri <trisulipras@gmail.com>
2023-11-05 17:38:50 +0700

    TBA-04: Handle Update Schedule
    - Adding new routes to handle update schedule HTTP request

--- routes/schedules.js
+++ routes/schedules.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const _ = require('lodash');
+
 const router = express.Router();
 const dbSchedule = require('../db/schedule');
 const dbUser = require('../db/user');
@@ -45,6 +47,73 @@ router.post('/', async (req, res) => {
         timestamp: new Date()
       }
     });
+  }
+});
+
+router.patch('/:id', async (req, res) => {
+  try {
+    const validator = await validate(
+      req.body,
+      dbSchedule.Rules,
+      ['creator', 'title', 'description']
+    );
+
+    if (!validator.isValid) {
+      res.status(400);
+      res.send({
+        data: {
+          errors: validator.errors
+        },
+        meta: {
+          message: 'Oops, something wrong with the inputs!',
+          timestamp: new Date()
+        }
+      });
+    } else {
+      const scheduleId = req.params.id;
+      const user = await dbUser.findByNameOrEmail(req.body.creator);
+      const schedule = await dbSchedule.findByCreatorAndId(scheduleId, user._id);
+
+      if (!schedule) {
+        throw new Error('invalid_user_id')
+      }
+
+      const result = await dbSchedule.update({
+        ..._.omit(req.body, ['creator']),
+        updatedAt: (new Date()).getTime(),
+      }, scheduleId);
+
+      res.status(200);
+      res.send({
+        data: result,
+        meta: {
+          message: 'Schedule updated successfully!',
+          timestamp: new Date()
+        }
+      });
+    }
+  } catch (e) {
+    if (e.message === 'invalid_user_id') {
+      res.status(400);
+      res.send({
+        data: {
+          message: `The user doesn\'t have authorized to update the schedule`,
+        },
+        meta: {
+          message: 'Oops, something wrong with the inputs!',
+          timestamp: new Date()
+        }
+      });
+    } else {
+      res.status(500)
+      res.send({
+        data: {},
+        meta: {
+          message: e.message,
+          timestamp: new Date()
+        }
+      })
+    }
   }
 });
 
